41c00660ed17dca45ae87316981f3377ae6deacf
puzzlemeter <bingo605@gmail.com>
2025-10-02 23:25:44 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/takashicompany/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/takashicompany/keymap.c
@@ -336,29 +336,63 @@ void oledkit_render_info_user(void) {
 ////////////////////////////
 
 // clang-format off
+/* Qwerty
+ * ,-------------------------------------------------------------------------.
+ * |  Q  |   W  |   E    |   R    |   T  |   Y  |   U  |   I  |   O  |  P    |
+ * |-----+------+--------+--------+------+------+------+------+------+-------|
+ * |  A  |   S  |   D    |   F    |   G  |   H  |   J  |   K  |   L  |  ;:   |
+ * |-----+------+--------+--------+------|------+------+------+------+-------|
+ * |  Z  |   X  |   C    |   V    |   B  |   N  |   M  |  ,<  |  .>  |  /?   |
+ * `-----+------+--------+--------+------+------+------+------+------+-------'
+ * | `   | Ctrl |Sh(かな) |Esc/Cmd | Tab  |Space | Enter| Bksp |      |Sh(英数)|
+ * `-------------------------------------------------------------------------'
+ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   LAYOUT_universal(
     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                              KC_Y,   KC_U,     KC_I,   KC_O,       KC_P,
-    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                              KC_H,   KC_J,     KC_K,   KC_L,       KC_ENT,
-    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                              KC_N,   KC_M,  KC_COMM, KC_DOT, RSFT_T(KC_SLSH),
-    KC_LGUI, KC_LALT,  KC_LCTL, LSFT_T(KC_ESC),LT(1,KC_TAB),LT(3,KC_SPC),    KC_BSPC, LT(2,KC_SCLN),   _______,  _______,_______,      KC_LNG2
+    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                              KC_H,   KC_J,     KC_K,   KC_L,       KC_SCLN,
+    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                              KC_N,   KC_M,  KC_COMM, KC_DOT,       KC_SLSH,
+    LALT_T(KC_GRAVE), KC_LCTL,  LSFT_T(KC_LNG1), LGUI_T(KC_ESCAPE),LT(1,KC_TAB),LT(2,KC_SPC),LT(1,KC_ENTER), LT(2,KC_BSPC),_______,_______,_______, RSFT_T(KC_LNG2)
   ),
 
+  /* Layer 1 (Fn)
+   * ,-------------------------------------------------------------------------.
+   * |  !  |   @  |   #    |   $    |   %  |   ^  |   &  |   *  |   (  |  )    |
+   * |-----+------+--------+--------+------+------+------+------+------+-------|
+   * | F1  |  F2  |   F3   |   F4   |  F5  |   =  |   +  |   \　|   [  |   ]   |
+   * |-----+------+--------+--------+------|------+------+------+------+-------|
+   * | F6  |  F7  |   F8   |   F9   | F10  |   -  |   _  | Pipe |   '  |   "   |
+   * `-----+------+--------+--------+------+------+------+------+------+-------'
+   * |     |      |        |        |      |      |      |      |      |  かな |
+   * `------------------------------------------------------------------------'
+   */
   LAYOUT_universal(
-    S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),                                    S(KC_6),      S(KC_7),       S(KC_8),     S(KC_9),   S(KC_0),
-    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                                     KC_F6,    S(KC_EQL),        KC_EQL,    KC_LBRC,   KC_RBRC,
-    KC_F6,    KC_F7,   KC_F8,   KC_F9,  KC_F10,                                     KC_F12,   S(KC_BSLS), KC_MINS  , KC_QUOT  , KC_RSFT   ,
-    _______, _______, _______, _______, _______, _______,                  TO(2) ,  TO(0)    , _______  , KC_RALT  , KC_RGUI  , S(KC_QUOT)
+    S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),         S(KC_6), S(KC_7),    S(KC_8),   S(KC_9), S(KC_0),
+    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_EQL,  S(KC_EQL),  KC_BSLS,   KC_LBRC, KC_RBRC,
+    KC_F6,    KC_F7,   KC_F8,   KC_F9,  KC_F10,          KC_MINUS,S(KC_MINUS),S(KC_BSLS),KC_QUOT, S(KC_QUOT),
+    _______, _______, _______, _______, _______,_______,_______,_______,_______,_______,_______, KC_LNG1
   ),
 
+  /* Layer 2 (Adjust)
+   * ,-------------------------------------------------------------------------.
+   * |CPI-1k|CPI-100|CPI+100 |CPI+1k| Shot | RGB  |Mode+ |Mode- |      |       |
+   * |------+-------+--------+------+------+------+------+------+------+-------|
+   * |  1   |   2   |   3    |   4  |   5  |  <-  |   v  |   ^  |  ->  |       |
+   * |------+-------+--------+------+------|------+------+------+------+-------|
+   * |  6   |   7   |   8    |   9  |   0  |Ctl<- |Ctl-> | PgUp | PgDn | Save  |
+   * `------+-------+--------+------+------+------+------+------+------+-------'
+   * |      | Reset |        | SCRL+| SCRL-|      |      |      |      |       |
+   * `-------------------------------------------------------------------------'
+   */
   LAYOUT_universal(
-    KC_GRV,    KC_1,    KC_2,     KC_3,   S(KC_MINS),                               KC_MINS,    KC_BTN1,     KC_UP,  KC_BTN2, KC_KB_MUTE,
-    KC_BSLS,   KC_4,    KC_5,     KC_6,   S(KC_SCLN),                               TO(4),    KC_LEFT,   KC_DOWN,  KC_RIGHT, LGUI(LCTL(KC_Q)),
-    LGUI(LSFT(KC_4)),  KC_7,    KC_8,     KC_9,        KC_0,                     RCTL(KC_LEFT),  RCTL(KC_RIGHT),   KC_PGUP,  KC_PGDN, _______  ,
-    _______, _______, _______, _______,   _______, _______,                TO(1),  _______ , _______ , _______ , _______ , KC_LNG1
+    CPI_D1K,CPI_D100, CPI_I100,CPI_I1K, LGUI(LSFT(KC_4)),  RGB_TOG,      RGB_MOD,        RGB_RMOD,_______, _______,
+    KC_1,   KC_2,    KC_3,     KC_4,   KC_5,             KC_LEFT,      KC_DOWN,           KC_UP,KC_RIGHT, _______,
+    KC_6,   KC_7,    KC_8,     KC_9,   KC_0,             LCTL(KC_LEFT),LCTL(KC_RIGHT),  KC_PGUP,KC_PGDN, KBC_SAVE,
+    _______, KBC_RST, _______, SCRL_DVI, SCRL_DVD, _______,_______,_______,_______,_______, _______, _______
   ),
 
+// 有効にしていない layer
   LAYOUT_universal(
     RGB_TOG  , LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RIGHT), LGUI(KC_A),                RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
@@ -366,9 +400,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______    , KBC_RST  , _______  , _______  , _______  , _______  ,      _______ ,  _______  , _______  , _______  , KBC_RST  , _______
   ),
 
+  /* Layer 4 (Click)
+   * ,-------------------------------------------------------------------------.
+   * |     |      |        |        |      |      |      |      |      |       |
+   * |-----+------+--------+--------+------+------+------+------+------+-------|
+   * |     |      | Btn2   | Scroll | Btn1 |      | Btn1 |Scroll| Btn2 |       |
+   * |-----+------+--------+--------+------|------+------+------+------+-------|
+   * |     |      |        |        |      |      |      |      |      |       |
+   * `-----+------+--------+--------+------+------+------+------+------+-------'
+   * |     |      |        |        |      |      |      |      |      |       |
+   * `-------------------------------------------------------------------------'
+   */
   LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______,                              _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______, KC_MY_BTN2, KC_MY_SCR, KC_MY_BTN1,                              _______  ,KC_MY_BTN1, KC_MY_SCR, KC_MY_BTN2  , _______  ,
+    _______  , _______, KC_MY_BTN2, KC_MY_SCR, KC_MY_BTN1,                            _______  ,KC_MY_BTN1, KC_MY_SCR, KC_MY_BTN2  , _______  ,
     _______  , _______  , _______  , _______  , _______,                              _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,      _______ ,  _______  , _______  , _______  , _______  , _______
   )
